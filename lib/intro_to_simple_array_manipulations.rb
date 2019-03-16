084ce4688faff6af4bc89a940400b7039e285ee8
Kevin Malazarte <kevin.malazarte@gmail.com>
2019-03-16 14:52:18 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,4 +26,12 @@ def using_concat(array_1, array_2)
   array_1.concat(array_2)
 end
 
-def using_insert
+def using_insert(array, element)
+  array.insert(element)[4]
+end
+
+def using_uniq(array)
+  array.uniq 
+end 
+
+
